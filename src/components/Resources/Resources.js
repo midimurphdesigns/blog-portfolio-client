9d29e3858c7798dd148be2a73fab97369ce8b5e4
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-29 23:30:07 -0700

    resources

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -5,8 +5,159 @@ import './Resources.css'
 export default function Resources(props) {
   return (
     <div className="page-container">
-      <h1>hello world!</h1>
-      <h3>Resources page under construction</h3>
+      <h1 className="resources-page-title">Web Development Resources</h1>
+      <div className="row">
+        <div className="category col-4">
+          <h3 className="category-title">General Web Development</h3>
+          <ul>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+          </ul>
+        </div>
+        <div className="category col-4">
+          <h3 className="category-title">General JavaScript</h3>
+          <ul>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+          </ul>
+        </div>
+        <div className="category col-4">
+          <h3 className="category-title">React JS</h3>
+          <ul>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+          </ul>
+        </div>
+      </div>
+
+      {/* ===== */}
+      {/* ROW 2 */}
+      {/* ===== */}
+
+      <div className="row">
+        <div className="category col-4">
+          <h3 className="category-title">HTML/CSS</h3>
+          <ul>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+          </ul>
+        </div>
+        <div className="category col-4">
+          <h3 className="category-title">Node JS/Express</h3>
+          <ul>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+          </ul>
+        </div>
+        <div className="category col-4">
+          <h3 className="category-title">MongoDB</h3>
+          <ul>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+          </ul>
+        </div>
+      </div>
+
+      {/* ===== */}
+      {/* ROW 3 */}
+      {/* ===== */}
+
+      <div className="row">
+        <div className="category col-4">
+          <h3 className="category-title">SQL</h3>
+          <ul>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+          </ul>
+        </div>
+        <div className="category col-4">
+          <h3 className="category-title">Design & UX</h3>
+          <ul>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+          </ul>
+        </div>
+        <div className="category col-4">
+          <h3 className="category-title">Color</h3>
+          <ul>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+            <li><a href="">article title</a></li>
+          </ul>
+        </div>
+      </div>
+
     </div>
   )
 }
