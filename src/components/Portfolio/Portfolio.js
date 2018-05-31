a46e789994c03de83f760f812ce870f5a0b87228
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-30 21:26:55 -0700

    styling

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,11 +2,15 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 
 import './Portfolio.css';
-import TopBar from './TopBar';
+import TopBar from '../TopBar/TopBar';
 import Items from './Items'
 
 export default function Portfolio(props) {
   return (
-    <Items />
+    <div>
+      <TopBar />
+      <h1 className="page-title">Full Stack Apps</h1>
+      <Items />
+    </div>
   )
 }
