2a9ca3ea4deb0a1749917a58d798a052e8eb1c3d
midimurphdesigns <midimurphdesigns@gmail.com>
2018-06-10 09:02:55 -0700

    removed scroll to top from portfolio page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 
-import ScrollTop from '../ScrollTop/ScrollTop'
-
 import './Portfolio.css';
 import TopBar from '../TopBar/TopBar';
 import FullStackItems from './FullStackItems'
