b5ce11b0a5e184d35c27d45ccfec5e62c50f1ccb
midimurphdesigns <midimurphdesigns@gmail.com>
2018-06-10 08:51:24 -0700

    updated resume

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -5,7 +5,7 @@ import './TopBar.css'
 export default function TopBar(props) {
   return (
     <div className="topbar-container">
-      <a href="https://drive.google.com/file/d/1pfhJxi2fI0PuzE1RIIIVzKjeyQGjedLQ/view?usp=sharing" target="_blank">Resume</a>
+      <a href="https://drive.google.com/file/d/19Oc6_PwzvxSH-7RnXrcKHvpVVfnIsdP7/view?usp=sharing" target="_blank">Resume</a>
       <span className="divider"> | </span>
       <a href="https://www.linkedin.com/in/midimurphdesigns/" target="_blank">LinkedIn</a>
       <span className="divider"> | </span>
