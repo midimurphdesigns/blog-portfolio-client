17edfca5bd565d7ce1360b3f4a1c8e2d5065abb7
midimurphdesigns <midimurphdesigns@gmail.com>
2018-07-18 11:25:05 -0700

    updated background color

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -36,62 +36,69 @@ const TemplateWrapper = ({ children, sideDrawerOpen, toggleDrawer }) => {
         <SideDrawer show={sideDrawerOpen} />
         <div
           style={{
-            margin: '0 auto',
-            maxWidth: 980,
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            height: '100%',
-            minHeight: '75vh',
+            background: `linear-gradient(rgba(207, 232, 253, 1), rgba(232, 244, 254, 0.5))`,
           }}
         >
-          <Media query={{ maxWidth: 848 }}>
-            {matches =>
-              matches ? (
-                <div
-                  style={{
-                    margin: '0 auto',
-                    maxWidth: 980,
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    height: '100%',
-                    padding: '25px',
-                  }}
-                >
-                  <div style={{ flex: 1 }}>{children()}</div>
-                </div>
-              ) : (
-                <div
-                  style={{
-                    margin: '0 auto',
-                    maxWidth: 980,
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    height: '100%',
-                    padding: '25px',
-                  }}
-                >
-                  <div style={{ flex: 2.5, paddingRight: '30px' }}>
-                    {children()}
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 980,
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              height: '100%',
+              minHeight: '75vh',
+            }}
+          >
+            <Media query={{ maxWidth: 848 }}>
+              {matches =>
+                matches ? (
+                  <div
+                    style={{
+                      margin: '0 auto',
+                      maxWidth: 980,
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      height: '100%',
+                      padding: '25px',
+                    }}
+                  >
+                    <div style={{ flex: 1 }}>{children()}</div>
                   </div>
+                ) : (
+                  <div
+                    style={{
+                      margin: '0 auto',
+                      maxWidth: 980,
+                      display: 'flex',
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      height: '100%',
+                      padding: '25px',
+                    }}
+                  >
+                    <div style={{ flex: 2.5, paddingRight: '30px' }}>
+                      {children()}
+                    </div>
 
-                  <div style={{ flex: 1 }}>
-                    <Sidebar
-                      title="Blog (Under Construction)"
-                      description="Articles on modern web apps. All articles are written by Kevin Murphy, Fullstack Web Development."
-                    />
-                    <Sidebar
-                      title="Author"
-                      description="Kevin Murphy is a Full-stack Web Developer specializing in React and Node.js based in Paradise Valley, AZ."
-                    />
+                    <div style={{ flex: 1 }}>
+                      <Sidebar
+                        title="Blog (Under Construction)"
+                        description="Articles on modern web apps. All articles are written by Kevin Murphy, Fullstack Web Development."
+                      />
+                      <Sidebar
+                        title="Author"
+                        description="Kevin Murphy is a Full-stack Web Developer specializing in React and Node.js based in Paradise Valley, AZ."
+                      />
+                    </div>
                   </div>
-                </div>
-              )
-            }
-          </Media>
+                )
+              }
+            </Media>
+          </div>
         </div>
+
         <Footer />
       </div>
     )
